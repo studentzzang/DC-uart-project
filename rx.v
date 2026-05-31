4cfddb3a1871e4403e152c387b3ce22c6c3e8f80
studentzzang <studentzzang@gmail.com>
2026-05-31 22:30:46 +0900

    버그 수정

--- rx.v
+++ rx.v
@@ -14,7 +14,7 @@
 
 // PROGRAM		"Quartus Prime"
 // VERSION		"Version 18.1.0 Build 625 09/12/2018 SJ Lite Edition"
-// CREATED		"Sun May 31 05:58:58 2026"
+// CREATED		"Sun May 31 22:28:42 2026"
 
 module rx(
 	rx_serial,
@@ -36,8 +36,8 @@ wire	[2:0] q;
 reg	[1:0] state;
 wire	SYNTHESIZED_WIRE_0;
 wire	SYNTHESIZED_WIRE_1;
+wire	SYNTHESIZED_WIRE_30;
 wire	SYNTHESIZED_WIRE_31;
-wire	SYNTHESIZED_WIRE_32;
 wire	SYNTHESIZED_WIRE_6;
 wire	SYNTHESIZED_WIRE_7;
 wire	SYNTHESIZED_WIRE_8;
@@ -55,7 +55,6 @@ wire	SYNTHESIZED_WIRE_22;
 wire	SYNTHESIZED_WIRE_24;
 wire	SYNTHESIZED_WIRE_26;
 wire	SYNTHESIZED_WIRE_28;
-wire	SYNTHESIZED_WIRE_30;
 
 
 
@@ -67,19 +66,19 @@ rx_fsm	b2v_inst(
 	.rx_serial(rx_serial),
 	.bit_index_is_7(SYNTHESIZED_WIRE_0),
 	.baud_tick(baud_tick),
-	.data_valid(SYNTHESIZED_WIRE_30),
+	
 	.clear_bit_index(SYNTHESIZED_WIRE_6),
-	.count_en(SYNTHESIZED_WIRE_31));
+	.count_en(SYNTHESIZED_WIRE_30));
 	defparam	b2v_inst.DATA = 2;
 	defparam	b2v_inst.IDLE = 0;
 	defparam	b2v_inst.START = 1;
 	defparam	b2v_inst.STOP = 3;
 
-assign	SYNTHESIZED_WIRE_17 = SYNTHESIZED_WIRE_1 & baud_tick & SYNTHESIZED_WIRE_31;
+assign	SYNTHESIZED_WIRE_17 = SYNTHESIZED_WIRE_1 & baud_tick & SYNTHESIZED_WIRE_30;
 
-assign	SYNTHESIZED_WIRE_7 = baud_tick & SYNTHESIZED_WIRE_31;
+assign	SYNTHESIZED_WIRE_7 = baud_tick & SYNTHESIZED_WIRE_30;
 
-assign	SYNTHESIZED_WIRE_32 = ;
+assign	SYNTHESIZED_WIRE_31 = ;
 
 
 
@@ -113,8 +112,15 @@ begin
 	end
 end
 
-assign	SYNTHESIZED_WIRE_8 = SYNTHESIZED_WIRE_9 & baud_tick & SYNTHESIZED_WIRE_31;
+assign	SYNTHESIZED_WIRE_8 = SYNTHESIZED_WIRE_9 & baud_tick & SYNTHESIZED_WIRE_30;
 
+
+always@(posedge SYNTHESIZED_WIRE_11)
+begin
+	begin
+	data_out_ALTERA_SYNTHESIZED[1] <= rx_serial;
+	end
+end
 
 
 always@(posedge SYNTHESIZED_WIRE_12)
@@ -141,6 +147,13 @@ begin
 end
 
 
+always@(posedge SYNTHESIZED_WIRE_15)
+begin
+	begin
+	data_out_ALTERA_SYNTHESIZED[5] <= rx_serial;
+	end
+end
+
 
 always@(posedge SYNTHESIZED_WIRE_16)
 begin
@@ -157,18 +170,17 @@ begin
 	end
 end
 
-assign	SYNTHESIZED_WIRE_11 = SYNTHESIZED_WIRE_18 & SYNTHESIZED_WIRE_31;
+assign	SYNTHESIZED_WIRE_11 = SYNTHESIZED_WIRE_18 & baud_tick & SYNTHESIZED_WIRE_30;
 
-assign	SYNTHESIZED_WIRE_12 = SYNTHESIZED_WIRE_20 & baud_tick & SYNTHESIZED_WIRE_31;
+assign	SYNTHESIZED_WIRE_12 = SYNTHESIZED_WIRE_20 & baud_tick & SYNTHESIZED_WIRE_30;
 
-assign	SYNTHESIZED_WIRE_13 = SYNTHESIZED_WIRE_22 & baud_tick & SYNTHESIZED_WIRE_31;
+assign	SYNTHESIZED_WIRE_13 = SYNTHESIZED_WIRE_22 & baud_tick & SYNTHESIZED_WIRE_30;
 
-assign	SYNTHESIZED_WIRE_14 = SYNTHESIZED_WIRE_24 & baud_tick & SYNTHESIZED_WIRE_31;
+assign	SYNTHESIZED_WIRE_14 = SYNTHESIZED_WIRE_24 & baud_tick & SYNTHESIZED_WIRE_30;
 
-assign	SYNTHESIZED_WIRE_15 = SYNTHESIZED_WIRE_26 & SYNTHESIZED_WIRE_31;
+assign	SYNTHESIZED_WIRE_15 = SYNTHESIZED_WIRE_26 & baud_tick & SYNTHESIZED_WIRE_30;
 
-assign	SYNTHESIZED_WIRE_16 = SYNTHESIZED_WIRE_28 & baud_tick & SYNTHESIZED_WIRE_31;
-
+assign	SYNTHESIZED_WIRE_16 = SYNTHESIZED_WIRE_28 & baud_tick & SYNTHESIZED_WIRE_30;
 
 assign	data_out = data_out_ALTERA_SYNTHESIZED;
 
