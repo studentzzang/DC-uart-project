7810392667072b4e92cd8b94ff56223cd84913b8
10719minjihoo <elereur0416@naver.com>
2026-06-10 01:05:33 +0900

    tx 수정

--- lpm_mux_8.v
+++ lpm_mux_8.v
@@ -44,7 +44,6 @@ module lpm_mux_8 (
 	data4,
 	data5,
 	data6,
-	data7,
 	sel,
 	result);
 
@@ -55,12 +54,10 @@ module lpm_mux_8 (
 	input	  data4;
 	input	  data5;
 	input	  data6;
-	input	  data7;
 	input	[2:0]  sel;
 	output	  result;
 
-	wire [0:0] sub_wire9;
-	wire  sub_wire8 = data7;
+	wire [0:0] sub_wire8;
 	wire  sub_wire7 = data6;
 	wire  sub_wire6 = data5;
 	wire  sub_wire5 = data4;
@@ -68,14 +65,14 @@ module lpm_mux_8 (
 	wire  sub_wire3 = data2;
 	wire  sub_wire2 = data1;
 	wire  sub_wire0 = data0;
-	wire [7:0] sub_wire1 = {sub_wire8, sub_wire7, sub_wire6, sub_wire5, sub_wire4, sub_wire3, sub_wire2, sub_wire0};
-	wire [0:0] sub_wire10 = sub_wire9[0:0];
-	wire  result = sub_wire10;
+	wire [6:0] sub_wire1 = {sub_wire7, sub_wire6, sub_wire5, sub_wire4, sub_wire3, sub_wire2, sub_wire0};
+	wire [0:0] sub_wire9 = sub_wire8[0:0];
+	wire  result = sub_wire9;
 
 	lpm_mux	LPM_MUX_component (
 				.data (sub_wire1),
 				.sel (sel),
-				.result (sub_wire9)
+				.result (sub_wire8)
 				// synopsys translate_off
 				,
 				.aclr (),
@@ -84,7 +81,7 @@ module lpm_mux_8 (
 				// synopsys translate_on
 				);
 	defparam
-		LPM_MUX_component.lpm_size = 8,
+		LPM_MUX_component.lpm_size = 7,
 		LPM_MUX_component.lpm_type = "LPM_MUX",
 		LPM_MUX_component.lpm_width = 1,
 		LPM_MUX_component.lpm_widths = 3;
@@ -99,7 +96,7 @@ endmodule
 // Retrieval info: PRIVATE: SYNTH_WRAPPER_GEN_POSTFIX STRING "0"
 // Retrieval info: PRIVATE: new_diagram STRING "1"
 // Retrieval info: LIBRARY: lpm lpm.lpm_components.all
-// Retrieval info: CONSTANT: LPM_SIZE NUMERIC "8"
+// Retrieval info: CONSTANT: LPM_SIZE NUMERIC "7"
 // Retrieval info: CONSTANT: LPM_TYPE STRING "LPM_MUX"
 // Retrieval info: CONSTANT: LPM_WIDTH NUMERIC "1"
 // Retrieval info: CONSTANT: LPM_WIDTHS NUMERIC "3"
@@ -110,7 +107,6 @@ endmodule
 // Retrieval info: USED_PORT: data4 0 0 0 0 INPUT NODEFVAL "data4"
 // Retrieval info: USED_PORT: data5 0 0 0 0 INPUT NODEFVAL "data5"
 // Retrieval info: USED_PORT: data6 0 0 0 0 INPUT NODEFVAL "data6"
-// Retrieval info: USED_PORT: data7 0 0 0 0 INPUT NODEFVAL "data7"
 // Retrieval info: USED_PORT: result 0 0 0 0 OUTPUT NODEFVAL "result"
 // Retrieval info: USED_PORT: sel 0 0 3 0 INPUT NODEFVAL "sel[2..0]"
 // Retrieval info: CONNECT: @data 0 0 1 0 data0 0 0 0 0
@@ -120,7 +116,6 @@ endmodule
 // Retrieval info: CONNECT: @data 0 0 1 4 data4 0 0 0 0
 // Retrieval info: CONNECT: @data 0 0 1 5 data5 0 0 0 0
 // Retrieval info: CONNECT: @data 0 0 1 6 data6 0 0 0 0
-// Retrieval info: CONNECT: @data 0 0 1 7 data7 0 0 0 0
 // Retrieval info: CONNECT: @sel 0 0 3 0 sel 0 0 3 0
 // Retrieval info: CONNECT: result 0 0 0 0 @result 0 0 1 0
 // Retrieval info: GEN_FILE: TYPE_NORMAL lpm_mux_8.v TRUE
